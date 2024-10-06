4554b803955abfb45423a7067b0d270887e873ac
kostiantyn1024 <160493886+kostiantyn1024@users.noreply.github.com>
2024-10-06 08:54:52 +0200

    hw 30_09_03

---
+++ homework30.09_n03.js
@@ -0,0 +1,18 @@
+let forPass = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"]
+
+let pass = ""
+
+function createPassword(lightPassword){
+    for (let i = 0; i <lightPassword; i++){
+        let x = Math.floor(Math.random()*67)+1
+    pass += forPass[x]
+    }
+    console.log("Your password: " + pass)
+}
+
+
+createPassword(140);
+
